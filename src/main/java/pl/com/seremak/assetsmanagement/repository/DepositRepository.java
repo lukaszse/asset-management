b675de800ce945532d244cb7e5d553d197a75d14
lseremak <llseremak@gmail.com>
2022-11-20 22:23:19 +0100

    add transaction client and transaction creation, update and deletion after deposit event

--- src/main/java/pl/com/seremak/assetsmanagement/repository/DepositRepository.java
+++ src/main/java/pl/com/seremak/assetsmanagement/repository/DepositRepository.java
@@ -11,6 +11,5 @@ public interface DepositRepository extends ReactiveCrudRepository<Deposit, Strin
 
     Flux<Deposit> findAllByUsername(final String username);
     Mono<Deposit> findByUsernameAndName(final String username, final String name);
-
     Mono<Deposit> deleteByUsernameAndName(final String username, final String name);
 }
